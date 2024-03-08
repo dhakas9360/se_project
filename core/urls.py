3116dff798786a41720d717e0b1b2616e9eec8e2
Ankit Dhaka <dhakaankit357@gmail.com>
2024-03-08 14:46:30 +0530

    added pages

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
-
+from .import views
 urlpatterns = [
-    # path('', ),
+    path('',views.home,name="home"),
+    path('register',views.signup,name="signup"),
+    path('login',views.login,name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+ return render(request,'website/homepage.html')
+
+def login(request):
+ return render(request,'website/login.html')
+
+
+def signup(request):
+ return render(request,'website/signup.html')
