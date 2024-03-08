4117cf66dd40f364a8bc68d5149255444951be18
Ankit Dhaka <dhakaankit357@gmail.com>
2024-03-09 02:01:07 +0530

    js fix

--- core/static/js/login.js
+++ core/static/js/login.js
@@ -1,18 +1,15 @@
-/// for login and signup pages
-
 document.getElementById('changeBackgroundColorButton').addEventListener('click', function() {
   const rootStyle = document.documentElement.style;
   const currentColor = rootStyle.getPropertyValue('--bgc');
-  const el = document.getElementById('changeBackgroundColorButton')
+  const el = document.getElementById('changeBackgroundColorButton');
   // Toggle between different colors
-  if (currentColor != '#f4ffb8') {
+  if (currentColor === '#181818') {
     rootStyle.setProperty('--bgc', '#f4ffb8');
-    el.classList.remove('fa-moon');
-    el.classList.add('fa-sun');
-    
-  } else {
-    rootStyle.setProperty('--bgc', '#181818');
     el.classList.remove('fa-sun');
     el.classList.add('fa-moon');
+  } else {
+    rootStyle.setProperty('--bgc', '#181818');
+    el.classList.remove('fa-moon');
+    el.classList.add('fa-sun');
   }
 });
