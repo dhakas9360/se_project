af96f9337c23160bf021f183c4ac4762787f8a94
Ankit Dhaka <dhakaankit357@gmail.com>
2024-03-11 01:26:50 +0530

    added pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './App'
+import {RouterProvider , createBrowserRouter,} from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Homepage from './pages/Homepage'
+import Services from './pages/Services'
+import About from './pages/About'
+
+const router = createBrowserRouter([
+  {path:"/", element:<Homepage/>},
+  {path:"/login", element:<Login/>},
+  {path:"/signup", element:<Signup/>},
+  {path:"/about", element:<About/>},
+  {path:"/services", element:<Services/>}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
