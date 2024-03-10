9ec3198a4db1458110eaa84b42907a3067bf7f00
Ankit Dhaka <dhakaankit357@gmail.com>
2024-03-11 01:28:48 +0530

    added navbar

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import {Link,NavLink} from 'react-router-dom'
+import Navbar from '../components/Navbar'
 function Homepage() {
   return (
     <>
-   <div className='text-center'>
-    <button className='bg-pink-200 rounded-lg p-2'><Link to="/login" >Login</Link></button>
-    <button className='bg-pink-200 rounded-lg p-2'><Link to="/signup" >SignUp</Link></button>
-    <button className='bg-pink-200 rounded-lg p-2'><Link to="/about" >About</Link></button>
-    <button className='bg-pink-200 rounded-lg p-2'><Link to="/services" >Services</Link></button>
-    <button className='bg-pink-200 rounded-lg p-2'><NavLink to="/" className={({isActive})=>`${isActive ? "text-blue underline":"text-black"}`}>Home</NavLink></button>
-   </div>
+  <Navbar/>
     </>
   )
 }
