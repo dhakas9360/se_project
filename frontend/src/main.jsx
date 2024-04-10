e98174f50ae0839578d968810886a23cd10ecfc8
Ankit Dhaka <dhakaankit357@gmail.com>
2024-04-11 02:25:35 +0530

    done admin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import {RouterProvider , createBrowserRouter,} from 'react-router-dom'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Homepage from './pages/Homepage'
-import Services from './pages/Services'
-import About from './pages/About'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Homepage from "./pages/Homepage";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import User_dashboard from "./pages/Sidebar";
 
 const router = createBrowserRouter([
-  {path:"/", element:<Homepage/>},
-  {path:"/login", element:<Login/>},
-  {path:"/signup", element:<Signup/>},
-  {path:"/about", element:<About/>},
-  {path:"/services", element:<Services/>}
-])
+  { path: "/", element: <Homepage /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/about", element: <About /> },
+  { path: "/services", element: <Services /> },
+  { path: "/user_dashboard", element: <User_dashboard /> },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
