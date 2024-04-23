5003b886fac7d72fa7082a480c40da9bcbcfb4cf
Ankit Dhaka <dhakaankit357@gmail.com>
2024-04-24 01:37:21 +0530

    fix button click

--- parking-master/parking-master/vpms/users/booking.php
+++ parking-master/parking-master/vpms/users/booking.php
@@ -185,7 +185,11 @@ if (isset($_GET['city'])) {
             alertElement.style.display = 'none';
         }
     </script>
-
+ <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+        <script src="../admin/assets/js/main.js"></script>
 </body>
 
 </html>
